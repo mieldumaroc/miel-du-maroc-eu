bc28d80161d7bd963084ddd83596a8a7222689d3
mieldumaroc <276617555+mieldumaroc@users.noreply.github.com>
2026-04-17 18:45:34 +0100

    fix: blog duplicate imports

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,4 +1,3 @@
-import { BLOG_POSTS } from '../data/products';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
